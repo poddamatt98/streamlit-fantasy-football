bdc64557363d5072c079d46697fd588bfff3450d
poddamatt98 <podda.matt@gmail.com>
2025-01-26 18:45:53 +0100

    first commit

---
+++ dashboard.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+from utils import get_github_files, read_remote_csv, read_remote_yaml, process_data, highlight_rows
+import yaml
+import os
+
+pd.options.display.float_format = '{:.2f}'.format
+
+st.title('Fantacalcio manager')
+
+data_path = 'data/release_prices'
+config_path = 'data/config'
+
+# files = get_github_files(data_path)
+files = os.listdir(data_path)
+
+seasons = set([f.split('_')[0] for f in files])
+
+season_col, league_col, team_col = st.columns(3)
+
+selected_season = season_col.selectbox('Stagione', options=seasons)
+
+leagues = set([f.split('_')[1] for f in files if f.startswith(selected_season)])
+
+selected_league = league_col.selectbox('Lega', options=leagues)
+
+# read file
+data_file = [f for f in files if f.startswith(f'{selected_season}_{selected_league}')][0]
+config_file = '_'.join(data_file.split('_')[:-1]) + '_config.yaml'
+
+data = pd.read_csv(data_path + '/' + data_file)
+with open(config_path + '/' + config_file, 'r') as f:
+    config = yaml.safe_load(f)
+
+processed_data = process_data(data, config)
+
+teams = processed_data.team.unique()
+selected_team = team_col.selectbox('Squadra', options=teams)
+
+team_data = processed_data[processed_data.team == selected_team].drop('team', axis=1).reset_index(drop=True)
+print(team_data.columns)
+team_data = team_data.rename({
+    'role': 'Ruolo',
+    'name': 'Nome',
+    'buyPrice': 'P. Acq.',
+    'startingFCPrice': 'Quot. Iniz.',
+    'actualFCPrice': 'Quot. Att.',
+    'releasePrice': 'Prezzo di Svincolo'
+}, axis=1)
+
+table = team_data.style.apply(highlight_rows, axis=1)
+
+st.table(table)
---
+++ utils.py
@@ -0,0 +1,47 @@
+import requests
+import io
+import pandas as pd
+import streamlit as st
+import yaml
+
+
+@st.cache_data
+def get_github_files(path):
+    """Retrieve a list of files in a GitHub repository subfolder."""
+
+    response = requests.get(path, headers={})
+    if response.status_code == 200:
+        data = response.json()
+        files = [item['name'] for item in data if item['type'] == 'file']
+        return files
+    else:
+        raise Exception(f"Error {response.status_code}: {response.json()}")
+
+# @st.cache_data
+def read_remote_yaml(path):
+    download_url = requests.get(path).json()['download_url']
+    data_str = requests.get(download_url).content
+    
+    config = yaml.safe_load(io.StringIO(data_str.decode('utf-8')))
+
+    return config
+
+# @st.cache_data
+def read_remote_csv(path):
+    download_url = requests.get(path).json()['download_url']
+    data_str = requests.get(download_url).content
+    data = pd.read_csv(io.StringIO(data_str.decode('utf-8')))
+
+    return data
+
+def process_data(data, config):
+    data['Rapporto'] = (data['actualFCPrice'] / data['startingFCPrice']).clip(upper=config['ratio_ub'], lower=config['ratio_lb']).round(int(config['precision']))
+    data['releasePrice'] = (data['Rapporto'] * data['buyPrice']).round(0).clip(lower=1).astype(int)
+    
+    return data
+    
+def highlight_rows(row):
+    if row["Rapporto"] >= 1:
+        return ['background-color: green'] * len(row)  # Apply light green to the entire row
+    else:
+        return ['background-color: red'] * len(row)  # Apply light coral to the entire row
